baba74c1186e2194a9af8adca6b643263d4988ca
RahulSChauhan50 <rahulschauhan50@gmail.com>
2022-03-26 02:10:19 +0530

    Updated backend functions

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,54 @@ const io = new Server(server, {
   },
 });
 
+const { height, width } = robot.getScreenSize();
+
+let event = "move";
+let shouldClick = true;
+let dragToggle = false;
+
 io.on("connection", (socket) => {
   console.log("A user connected");
-  socket.on("coordinates", (data) => {
-    robot.moveMouse(data.x, data.y);
+  socket.on("event", (d) => {
+    // console.log(d.key, d.coordinates.x, d.coordinates.y);
+    event = d.key;
+    switch (event) {
+      case "move": {
+        if (dragToggle) {
+          robot.mouseToggle("up");
+          dragToggle = false;
+        }
+        robot.moveMouse(d.coordinates.x, d.coordinates.y);
+        shouldClick = true;
+
+        break;
+      }
+      case "drag": {
+        if (!dragToggle) {
+          dragToggle = true;
+          robot.mouseToggle("down");
+        }
+        robot.moveMouse(d.coordinates.x, d.coordinates.y);
+        shouldClick = true;
+        break;
+      }
+      case "click": {
+        if (dragToggle) {
+          robot.mouseToggle("up");
+          dragToggle = false;
+        }
+        if (shouldClick) {
+          robot.mouseClick();
+          shouldClick = false;
+        }
+        break;
+      }
+      default: {
+        robot.moveMouse(d.coordinates.x, d.coordinates.y);
+        shouldClick = true;
+        break;
+      }
+    }
   });
 
   socket.on("disconnect", () => {
